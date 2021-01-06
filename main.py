0edb44d365c859900839ad417f6f561ad18e944f
Olsi Sadiku <olsi_sadiku@hotmail.com>
2021-01-06 09:48:07 -0600

    O

---
+++ main.py
@@ -0,0 +1,29 @@
+import tkinter
+import socket
+import selectors
+
+
+HOST = 'localhost'
+PORT = 8002
+
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
